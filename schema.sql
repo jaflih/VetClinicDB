66c6e9c73108ab703bfe8e6b76219f5b3d1e8bce
jihaneh <jijih@hotmail.fr>
2022-03-21 14:45:10 +0100

    Change REAL by DECIMAL and TEXT by VARCHAR(255)

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
 /* Database schema to keep the structure of entire database. */
 
 create table animals(id INT PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY, 
-					 name TEXT NOT NULL, 
+					 name VARCHAR(255) NOT NULL, 
 					 date_of_birth DATE NOT NULL, 
 					 escape_attempts INT DEFAULT 0, 
 					 neutered BOOLEAN DEFAULT false, 
-					 weight_kg REAL
+					 weight_kg DECIMAL
 					);
